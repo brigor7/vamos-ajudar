f9527d18a6a8b76f586fc0b495ef68a9590b462d
=Bruno Igor <=brigor7@gmail.com>
2021-01-19 16:38:43 -0300

    Criado post em userController

---
+++ backend/src/controller/userController.js
@@ -0,0 +1,40 @@
+const api = require('../database/connection');
+
+module.exports = {
+  get(_, res) {
+    res.send('user get acessado');
+  },
+  async create(request, response) {
+    const {
+      nome,
+      apelido,
+      nascimento,
+      sexo,
+      email,
+      whatsapp,
+      password,
+      thumbnail,
+      isAdmin,
+      cidade,
+      uf,
+    } = request.body;
+    try {
+      await api('users').insert({
+        nome,
+        apelido,
+        nascimento,
+        sexo,
+        email,
+        whatsapp,
+        password,
+        thumbnail,
+        isAdmin,
+        cidade,
+        uf,
+      });
+      return response.status(201).json({ nome });
+    } catch (error) {
+      console.info('#Erro inserir user' + error);
+    }
+  },
+};
