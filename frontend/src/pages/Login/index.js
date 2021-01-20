e8533b0071acc83cfdc0afc0ebef9b1977287ee4
=Bruno Igor <=brigor7@gmail.com>
2021-01-20 17:38:47 -0300

    Create login page

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { FiUserX, FiUserPlus } from 'react-icons/fi';
 
-export default function Login() {
-  return <h1>Login</h1>;
+function App() {
+  return (
+    <>
+      <h1>Login</h1>
+      <form action="">
+        <input placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+        <button>Entrar</button>
+      </form>
+      <div className="form-footer">
+        <a href="#">
+          <FiUserPlus size={12} color="#fff" />
+          Novo usuário
+        </a>
+        <a href="#">
+          <FiUserX size={12} color="#fff" /> Esqueci a senha
+        </a>
+      </div>
+    </>
+  );
 }
+
+export default App;
