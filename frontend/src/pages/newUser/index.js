b6f4c39e43335b25875760af5946b187c5347720
=Bruno Igor <=brigor7@gmail.com>
2021-01-21 15:29:52 -0300

    Create page newUser

---
+++ frontend/src/pages/newUser/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { FiArrowLeft, FiCamera } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+export default function NewUser() {
+  return (
+    <div>
+      <h1>Novo cadastro</h1>
+      <form action="">
+        <div className="formContainer">
+          <div className="personal-content">
+            <span className="thumb-container">
+              <label id="thumbnail">
+                <input type="file" />
+              </label>
+            </span>
+            <input placeholder="Seu nome Completo" />
+            <input placeholder="Como quer ser chamado?" />
+            <input
+              id="nascimento"
+              placeholder="Data de nascimento com dd/mm/aaaa"
+            />
+            <span class="genero">
+              <input
+                type="radio"
+                value="0"
+                name="campo-radio"
+                id="campo-radio1"
+              />
+              <label for="campo-radio1">Masculino</label>
+              <input
+                type="radio"
+                value="0"
+                name="campo-radio"
+                id="campo-radio2"
+              />
+              <label for="campo-radio2">Feminino</label>
+            </span>
+          </div>
+          <div className="contacts-content">
+            <input type="email" placeholder="Seu melhor email" />
+            <input placeholder="Seu whatsapp" />
+            <span className="residence-content">
+              <input id="cidade" placeholder="Cidade onde mora" />
+              <input id="uf" placeholder="UF" />
+            </span>
+            <input type="password" placeholder="Senha" />
+            <input type="password" placeholder="Repita sua senha" />
+          </div>
+        </div>
+        <span className="button">
+          <button id="cadastrar">Cadastrar</button>
+        </span>
+      </form>
+      <div className="form-footer">
+        <Link to="/">
+          <FiArrowLeft size={12} color="#fff" />
+          Voltar
+        </Link>
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import NewUser from './pages/newUser';
+import Check from './pages/Check';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/newUser" component={NewUser} />
+        <Route path="/check" component={Check} />
       </Switch>
     </BrowserRouter>
   );
