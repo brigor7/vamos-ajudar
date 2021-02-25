81a88c9789e2893e47f79ccd7a98c76b5c9095f6
=Bruno Igor <=brigor7@gmail.com>
2021-02-24 22:35:23 -0300

    Ajuste de rotas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import NewUser from './pages/NewUser';
+import LoginForm from './components/Login/LoginForm';
 import Main from './pages/Main';
-import Menu from './components/Menu';
-import Footer from './components/Footer';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/newUser" component={NewUser} />
-        <Route path="/main" component={Main} />
-        <Route path="/menu" component={Menu} />
-        <Route path="/footer" component={Footer} />
+        <Route path="/" exact component={LoginForm} />
+        <Route path="/main" exact component={Main} />
       </Switch>
     </BrowserRouter>
   );
