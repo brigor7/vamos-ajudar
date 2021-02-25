0329c8dd0651312b13299f19c2bbe3f47db5cbab
=Bruno Igor <=brigor7@gmail.com>
2021-02-25 09:23:04 -0300

    Methods designed to create and verify encryption

---
+++ backend/src/helper/encrypt.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+
+const SECRET = 'vamosajudar';
+
+module.exports = {
+  async hash(password) {
+    return new Promise((resolve, reject) => {
+      crypto.scrypt(password, SECRET, 64, (err, derivedKey) => {
+        if (err) reject(err);
+        resolve(SECRET + ':' + derivedKey.toString('hex'));
+      });
+    });
+  },
+
+  async verify(password, key) {
+    return new Promise((resolve, reject) => {
+      crypto.scrypt(password, SECRET, 64, (err, derivedKey) => {
+        if (err) reject(err);
+        resolve(key == derivedKey.toString('hex'));
+      });
+    });
+  },
+};
