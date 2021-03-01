4e56328730d1231903988cbc5aa8a7381581023d
=Bruno Igor <=brigor7@gmail.com>
2021-03-01 17:55:16 -0300

    Inclusão de link e coloração svg

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import { ReactComponent as LogoTransparente } from '../../assets/logoTransparente.svg';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <LogoTransparente />
+      <Link className={styles.logo} to="/" aria-label="Home">
+        <LogoTransparente />
+      </Link>
       <p>Alguns direitos reservados</p>
     </footer>
   );
