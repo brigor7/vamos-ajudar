08267980cf7395fc3f38bf562f6c81f5ec079d30
=Bruno Igor <=brigor7@gmail.com>
2021-03-01 16:44:07 -0300

    Incluir components header e footer antes das rotas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,15 +3,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoginForm from './components/Login/LoginForm';
 import Main from './pages/Main';
 import { UserProvider } from './context/UserContext';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <UserProvider>
-        <Switch>
-          <Route path="/" exact component={LoginForm} />
-          <Route path="/main" exact component={Main} />
-        </Switch>
+        <Header />
+        <main className="AppBody">
+          <Switch>
+            <Route path="/" exact component={LoginForm} />
+            <Route path="/main" exact component={Main} />
+          </Switch>
+        </main>
+        <Footer />
       </UserProvider>
     </BrowserRouter>
   );
