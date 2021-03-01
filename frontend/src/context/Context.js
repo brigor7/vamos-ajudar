2b451aec6824bb07f287d6678da399a0f2d02684
=Bruno Igor <=brigor7@gmail.com>
2021-03-01 16:46:13 -0300

    Create component UserContext

--- frontend/src/context/Context.js
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from 'react';
-
-const vamosContext = createContext();
-
-export const vamosProvider = ({ children }) => {
-  return <vamosContext.Provider>{children}</vamosContext.Provider>;
-};
-
-export default vamosContext;
