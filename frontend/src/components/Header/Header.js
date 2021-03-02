4477d060e2c3051d4697cbbfbc3b908fca15f7e8
=Bruno Igor <=brigor7@gmail.com>
2021-03-02 18:29:58 -0300

    Mostrando o nome do usuario no Header após logon

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Header.module.css';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
 
 const Header = () => {
+  const { data } = useContext(UserContext);
+  const { user } = JSON.parse(data);
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <Link className={styles.logo} to="/" aria-label="Home">
           <Logo />
         </Link>
-        <Link className={styles.login} to="/login" aria-label="Home">
-          Bem vindo(a) Zé das Couves
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/login" aria-label="Home">
+            Bem vindo(a) {user.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login" aria-label="Home">
+            Login/Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
