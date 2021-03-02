f583e51c2d71bca97bb80b725580719ffb7c6a0d
=Bruno Igor <=brigor7@gmail.com>
2021-03-01 23:01:45 -0300

    Inclusão de rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
-import Routes from './routes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Routes />
+      <BrowserRouter>
+        <UserProvider>
+          <Header />
+          <main className="AppBody">
+            <Routes>
+              <Route path="/login/*" element={<Login />} />
+            </Routes>
+          </main>
+          <Footer />
+        </UserProvider>
+      </BrowserRouter>
     </div>
   );
 }
