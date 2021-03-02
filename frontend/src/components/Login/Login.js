df1e8521914eb57702b9d27770cda16f0939e359
=Bruno Igor <=brigor7@gmail.com>
2021-03-01 23:02:58 -0300

    Estilização de Login

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Login.module.css';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
@@ -10,12 +10,12 @@ const Login = () => {
   return (
     <section className={styles.login}>
       <div className={styles.form}>
-        <Switch>
-          <Route path="/" element={LoginForm} />
-          <Route path="/criar" element={LoginCreate} />
-          <Route path="/perdeu" element={LoginPasswordLost} />
-          <Route path="/resetar" element={LoginPasswordReset} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/criar" element={<LoginCreate />} />
+          <Route path="/perdeu" element={<LoginPasswordLost />} />
+          <Route path="/resetar" element={<LoginPasswordReset />} />
+        </Routes>
       </div>
     </section>
   );
