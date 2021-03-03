669a91f20daee1146f875d288306623a3bc62a98
=Bruno Igor <=brigor7@gmail.com>
2021-03-03 16:00:06 -0300

    Inclusão de form e inputs em Login Create

--- frontend/src/components/Login/LoginCreate.js
+++ frontend/src/components/Login/LoginCreate.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import style from './LoginForm.module.css';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import './LoginCreate.module.css';
 
 const LoginCreate = () => {
-  return <div>Login Create</div>;
+  const [nome, setNome] = useState('');
+  const [apelido, setApelido] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form>
+        <Input label="Nome" type="text" name={'nome'} {...nome} required />
+        <Input
+          label="Apelido"
+          type="apelido"
+          name={'apelido'}
+          {...apelido}
+          required
+        />
+        <Input label="Email" type="email" nome={'email'} {...email} required />
+        <Input
+          label="Senha"
+          type="password"
+          nome={'password'}
+          {...password}
+          required
+        />
+
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
