0c3c26792ff40a3b067c1a1bb9b7bc96b912db32
=Bruno Igor <=brigor7@gmail.com>
2021-03-03 17:05:11 -0300

    Create Input de arquivo com estilização para foto

--- frontend/src/components/Login/LoginCreate.js
+++ frontend/src/components/Login/LoginCreate.js
@@ -1,19 +1,49 @@
 import React, { useState, useMemo } from 'react';
 import style from './LoginForm.module.css';
+import { ReactComponent as Camera } from '../../assets/lnr-camera.svg';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
-import './LoginCreate.module.css';
+import stylesThumb from './LoginCreate.module.css';
 
 const LoginCreate = () => {
   const [nome, setNome] = useState('');
   const [apelido, setApelido] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [thumbnail, setThumbnail] = useState('');
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
-      <form>
+
+      <form className={style.form} onSubmit={handleSubmit}>
+        <span className={stylesThumb.thumbContainer}>
+          <span id={stylesThumb.thumbnail}>
+            <label
+              id="thumbnail"
+              style={{ backgroundImage: `url(${preview})` }}
+              className={thumbnail ? 'has-thumbnail' : ''}
+            >
+              <input
+                type="file"
+                onChange={(e) => {
+                  setThumbnail(e.target.files[0]);
+                }}
+              />
+              <span className={stylesThumb.svg}>
+                <Camera />
+              </span>
+            </label>
+          </span>
+        </span>
         <Input label="Nome" type="text" name={'nome'} {...nome} required />
         <Input
           label="Apelido"
