729921b3b784c75e22d3fc50e5cd847940063331
Bruno Igor <brigor7@gmail.com>
2021-03-19 14:59:46 -0300

    Refactory

--- frontend/src/components/Family/Family.js
+++ frontend/src/components/Family/Family.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
-import { Route, Routes } from "react-router";
 import Table from "../Forms/Table";
-import FamilyContext, { FamilyProvider } from "../../context/FamilyContext";
+import FamilyContext from "../../context/FamilyContext";
 import Error from "../helpers/Error";
 
 const Family = () => {
@@ -16,16 +15,16 @@ const Family = () => {
 
   const rota = "family";
   const head = ["Responsavel", "Contato", "Cidade"];
-  const ndata = data.map(({ family_id, responsavel, cidade, uf, whatsapp }) => {
-    return {
-      id: family_id,
-      col1: responsavel,
-      col2: whatsapp,
-      col3: cidade + " - " + uf,
-    };
-  });
-
-  console.log("nova data:", ndata);
+  const newData = data.map(
+    ({ family_id, responsavel, cidade, uf, whatsapp }) => {
+      return {
+        id: family_id,
+        col1: responsavel,
+        col2: whatsapp,
+        col3: cidade + " - " + uf,
+      };
+    }
+  );
 
   if (error) return <Error error={error} />;
 
@@ -34,7 +33,7 @@ const Family = () => {
       {loading ? (
         <p>Carregando...</p>
       ) : (
-        <Table data={ndata} head={head} rota={rota} />
+        <Table data={newData} head={head} rota={rota} />
       )}
     </div>
   );
